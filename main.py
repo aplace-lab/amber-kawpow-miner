27c608485a7cba8b12688145711429a0fda34da0
aplace-lab <m.gimmer84@gmail.com>
2024-10-13 17:25:00 +1030

    Bump version

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ DEFAULT_CONFIG = {
 }
 
 # Static variables
-VERSION = "0.2.2"
+VERSION = "0.2.3"
 LOG_FILE = "amber-kawpow-miner.log"
 GMINER_MINING_API_URL = "http://127.0.0.1:4068/stat"
 TEAMREDMINER_API_HOST = '127.0.0.1'
